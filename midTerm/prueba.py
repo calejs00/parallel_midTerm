4f3d58c4053f6c7fe8f58ea083bdbad1f15cfe41
Clara Alejos <calejs00@estudiantes.unileon.es>
2025-01-30 16:38:19 +0100

    dataset

---
+++ midTerm/prueba.py
@@ -0,0 +1,49 @@
+total_segundosSeq = 4742.92
+# Convertir el total de segundos a horas, minutos y segundos
+media = int(total_segundosSeq // 100)
+horas = int(total_segundosSeq // 3600)
+minutos = int((total_segundosSeq % 3600) // 60)
+segundos = total_segundosSeq % 60
+
+print(f"Tiempo total: {horas} horas, {minutos} minutos, {segundos:.2f} segundos")
+print(f"La media de segundos por contrasenna es: {media}")
+
+total_segundos2 = 4742.92
+# Convertir el total de segundos a horas, minutos y segundos
+media = int(total_segundos2 // 100)
+horas = int(total_segundos2 // 3600)
+minutos = int((total_segundos2 % 3600) // 60)
+segundos = total_segundos2 % 60
+
+print(f"Tiempo total: {horas} horas, {minutos} minutos, {segundos:.2f} segundos")
+print(f"La media de segundos por contrasenna es: {media}")
+
+total_segundos4 = 4597.31
+# Convertir el total de segundos a horas, minutos y segundos
+media = int(total_segundos4 // 100)
+horas = int(total_segundos4 // 3600)
+minutos = int((total_segundos4 % 3600) // 60)
+segundos = total_segundos4 % 60
+
+print(f"Tiempo total: {horas} horas, {minutos} minutos, {segundos:.2f} segundos")
+print(f"La media de segundos por contrasenna es: {media}")
+
+total_segundos8 = 4469.97
+# Convertir el total de segundos a horas, minutos y segundos
+media = int(total_segundos8 // 100)
+horas = int(total_segundos8 // 3600)
+minutos = int((total_segundos8 % 3600) // 60)
+segundos = total_segundos8 % 60
+
+print(f"Tiempo total: {horas} horas, {minutos} minutos, {segundos:.2f} segundos")
+print(f"La media de segundos por contrasenna es: {media}")
+
+total_segundos16 = 2934.88
+# Convertir el total de segundos a horas, minutos y segundos
+media = int(total_segundos16 // 100)
+horas = int(total_segundos16 // 3600)
+minutos = int((total_segundos16 % 3600) // 60)
+segundos = total_segundos16 % 60
+
+print(f"Tiempo total: {horas} horas, {minutos} minutos, {segundos:.2f} segundos")
+print(f"La media de segundos por contrasenna es: {media}")
